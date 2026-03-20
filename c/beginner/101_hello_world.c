382a97b8ca8d876669eb57c42df3ace46e434b1e
godffscom-GitRace <godffscom@gmail.com>
2026-03-20 23:28:20 +0900

    Create 101_hello_world.c

---
+++ c/beginner/101_hello_world.c
@@ -0,0 +1,17 @@
+// [101] Hello World - 첫 번째 C 프로그램
+// 레벨: 1 | C 언어의 기본 구조와 printf 함수를 배웁니다
+
+#include <stdio.h>
+
+int main() {
+    // printf: 화면에 텍스트를 출력하는 함수
+    printf("Hello, World!\n");
+    printf("안녕하세요, C 언어!\n");
+
+    // \n은 줄바꿈(newline) 문자
+    printf("첫 번째 줄\n");
+    printf("두 번째 줄\n");
+
+    // return 0: 프로그램이 정상 종료됨을 의미
+    return 0;
+}
