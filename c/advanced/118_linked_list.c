918674cb7ecc3cdb3c09d46770eb268af96c106e
godffscom-GitRace <godffscom@gmail.com>
2026-04-24 01:26:06 +0900

    Update 118_linked_list.c

--- c/advanced/118_linked_list.c
+++ c/advanced/118_linked_list.c
@@ -3,126 +3,129 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-// 노드 구조체
+// Node structure
 typedef struct Node {
     int data;
-    struct Node *next;  // 다음 노드를 가리키는 포인터
+    struct Node* next;
 } Node;
 
-// 연결 리스트 구조체
+// Linked list
 typedef struct {
-    Node *head;
+    Node* head;
     int size;
 } LinkedList;
 
-// 함수 선언
-LinkedList* createList();
-void pushFront(LinkedList *list, int data);
-void pushBack(LinkedList *list, int data);
-void deleteNode(LinkedList *list, int data);
-void printList(LinkedList *list);
-void freeList(LinkedList *list);
 
-int main() {
-    LinkedList *list = createList();
-
-    // 뒤에 추가
-    pushBack(list, 10);
-    pushBack(list, 20);
-    pushBack(list, 30);
-    pushBack(list, 40);
-
-    printf("초기 리스트: ");
-    printList(list);
-
-    // 앞에 추가
-    pushFront(list, 5);
-    printf("앞에 5 추가: ");
-    printList(list);
-
-    // 노드 삭제
-    deleteNode(list, 20);
-    printf("20 삭제 후: ");
-    printList(list);
-
-    deleteNode(list, 5);
-    printf("5 삭제 후: ");
-    printList(list);
-
-    printf("크기: %d\n", list->size);
-
-    freeList(list);
-    printf("메모리 해제 완료\n");
-
-    return 0;
-}
-
-LinkedList* createList() {
-    LinkedList *list = (LinkedList*)malloc(sizeof(LinkedList));
+// create list
+LinkedList* create_list() {
+    LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
     list->head = NULL;
     list->size = 0;
     return list;
 }
 
-void pushFront(LinkedList *list, int data) {
-    Node *newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = list->head;
-    list->head = newNode;
+
+// push front
+void push_front(LinkedList* list, int value) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = value;
+    node->next = list->head;
+
+    list->head = node;
     list->size++;
 }
 
-void pushBack(LinkedList *list, int data) {
-    Node *newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = NULL;
+
+// push back
+void push_back(LinkedList* list, int value) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = value;
+    node->next = NULL;
 
     if (list->head == NULL) {
-        list->head = newNode;
+        list->head = node;
     } else {
-        Node *curr = list->head;
-        while (curr->next != NULL) curr = curr->next;
-        curr->next = newNode;
+        Node* cur = list->head;
+        while (cur->next != NULL) {
+            cur = cur->next;
+        }
+        cur->next = node;
     }
+
     list->size++;
 }
 
-void deleteNode(LinkedList *list, int data) {
-    Node *curr = list->head;
-    Node *prev = NULL;
 
-    while (curr != NULL && curr->data != data) {
-        prev = curr;
-        curr = curr->next;
+// delete node
+void delete_node(LinkedList* list, int value) {
+    Node* cur = list->head;
+    Node* prev = NULL;
+
+    while (cur != NULL && cur->data != value) {
+        prev = cur;
+        cur = cur->next;
     }
 
-    if (curr == NULL) return;  // 못 찾음
+    if (cur == NULL) return;
 
-    if (prev == NULL) list->head = curr->next;
-    else prev->next = curr->next;
+    if (prev == NULL) {
+        list->head = cur->next;
+    } else {
+        prev->next = cur->next;
+    }
 
-    free(curr);
+    free(cur);
     list->size--;
 }
 
-void printList(LinkedList *list) {
-    Node *curr = list->head;
-    while (curr != NULL) {
-        printf("%d", curr->data);
-        if (curr->next) printf(" -> ");
-        curr = curr->next;
+
+// print list
+void print_list(LinkedList* list) {
+    Node* cur = list->head;
+
+    while (cur != NULL) {
+        printf("%d", cur->data);
+        if (cur->next) printf(" -> ");
+        cur = cur->next;
     }
+
     printf(" -> NULL\n");
 }
 
-void freeList(LinkedList *list) {
-    Node *curr = list->head;
-    while (curr != NULL) {
-        Node *next = curr->next;
-        free(curr);
-        curr = next;
+
+// free list
+void free_list(LinkedList* list) {
+    Node* cur = list->head;
+
+    while (cur != NULL) {
+        Node* next = cur->next;
+        free(cur);
+        cur = next;
     }
+
     free(list);
+}
+
+
+// main
+int main() {
+    LinkedList* list = create_list();
+
+    push_back(list, 10);
+    push_back(list, 20);
+    push_back(list, 30);
+
+    print_list(list);
+
+    push_front(list, 5);
+    print_list(list);
+
+    delete_node(list, 20);
+    print_list(list);
+
+    printf("size: %d\n", list->size);
+
+    free_list(list);
+    return 0;
 }
