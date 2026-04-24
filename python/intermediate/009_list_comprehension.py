34c348ee9aa71273ecd5e1002dd17078c2111e05
godffscom-GitRace <godffscom@gmail.com>
2026-04-25 01:23:25 +0900

    Update 009_list_comprehension.py

--- python/intermediate/009_list_comprehension.py
+++ python/intermediate/009_list_comprehension.py
@@ -2,7 +2,7 @@
 # 레벨: 3 | Python의 강력한 기능인 리스트 컴프리헨션으로 간결한 코드를 작성합니다
 
 # 기본 문법: [표현식 for 변수 in 반복가능]
-print("⚡ List Comprehension Game\n")
+print("List Comprehension Game\n")
 
 # 1. Basic pattern
 squares = [x * x for x in range(1, 6)]
@@ -20,7 +20,7 @@ print("Labels:", labels)
 matrix = [[i * j for j in range(1, 4)] for i in range(1, 4)]
 print("Matrix:", matrix)
 
-print("\n🎯 Your turn!")
+print("\n Your turn!")
 n = int(input("Enter number: "))
 result = [x * x for x in range(1, n + 1)]
 print("Result:", result)
