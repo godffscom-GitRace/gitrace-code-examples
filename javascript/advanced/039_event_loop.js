a47ad6a39b3caf55b4c628972e9d6b630eb99555
godffscom-GitRace <godffscom@gmail.com>
2026-04-25 18:21:39 +0900

    Update 039_event_loop.js

--- javascript/advanced/039_event_loop.js
+++ javascript/advanced/039_event_loop.js
@@ -1,49 +1,48 @@
 // Event Loop
 
-// execution order: sync -> microtask -> macrotask
-console.log("=== 1. Execution order ===");
+console.log("1 start");
 console.log("start");
 
 setTimeout(() => {
-  console.log("setTimeout (macrotask)");
+  console.log("setTimeout");
 }, 0);
 
 Promise.resolve().then(() => {
-  console.log("Promise (microtask)");
+  console.log("promise");
 });
 
 console.log("end");
-// order: start => end => Promise => setTimeout
 
-// microtask queue vs macrotask queue
-console.log("\n=== 2. Task priority ===");
-setTimeout(() => console.log("A: setTimeout 1"), 0);
-setTimeout(() => console.log("B: setTimeout 2"), 0);
+console.log("2 tasks");
+
+setTimeout(() => console.log("A"), 0);
+setTimeout(() => console.log("B"), 0);
 
 Promise.resolve()
-  .then(() => console.log("C: Promise 1"))
-  .then(() => console.log("D: Promise 2"));
+  .then(() => console.log("C"))
+  .then(() => console.log("D"));
 
-Promise.resolve().then(() => console.log("E: Promise 3"));
-// order: C => E => D => A => B
+Promise.resolve().then(() => console.log("E"));
 
-// nested async
-console.log("\n=== 3. Nested async ===");
+console.log("3 nested");
+
 setTimeout(() => {
   console.log("timeout 1");
-  Promise.resolve().then(() => console.log("  -> promise inside timeout"));
+  Promise.resolve().then(() => console.log("promise in timeout"));
 }, 0);
 
 Promise.resolve().then(() => {
   console.log("promise 1");
-  setTimeout(() => console.log("  -> timeout inside promise"), 0);
+  setTimeout(() => console.log("timeout in promise"), 0);
 });
 
-// queueMicrotask
-console.log("\n=== 4. queueMicrotask ===");
+console.log("4 microtask");
+
 console.log("sync start");
-queueMicrotask(() => console.log("microtask 1"));
-queueMicrotask(() => console.log("microtask 2"));
-setTimeout(() => console.log("macrotask"), 0);
+
+queueMicrotask(() => console.log("micro 1"));
+queueMicrotask(() => console.log("micro 2"));
+
+setTimeout(() => console.log("macro"), 0);
+
 console.log("sync end");
-// order: sync start => sync end => microtask1 => microtask2 => macrotask
