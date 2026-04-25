ff1977c5f2c9e1fc7a46115a005699cf0b1018b2
godffscom-GitRace <godffscom@gmail.com>
2026-04-25 16:53:18 +0900

    Update 021_variables.js

--- javascript/beginner/021_variables.js
+++ javascript/beginner/021_variables.js
@@ -1,27 +1,28 @@
 // Variables: let, const, var
 
-// let - reassignable, block scope
 let age = 25;
 age = 26;
-console.log(`age: ${age}`);
+console.log("age: " + age);
 
-// const - not reassignable, block scope
 const name = "kim";
-console.log(`name: ${name}`);
+console.log("name: " + name);
 
-// var - function scope (not recommended)
 var score = 90;
 score = 95;
 
-// block scope difference
 if (true) {
   let blockLet = "block only";
   var blockVar = "anywhere";
 }
+
 console.log(blockVar);
 
-// hoisting
 var hoisted = "ok";
+
 const fruits = ["apple", "banana"];
 fruits.push("mango");
-console.log(fruits);
+
+console.log("fruits:");
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
