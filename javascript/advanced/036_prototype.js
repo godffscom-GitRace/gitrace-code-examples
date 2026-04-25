bddc087af6f42454891d58a0aa930e95e1dca451
godffscom-GitRace <godffscom@gmail.com>
2026-04-25 18:12:45 +0900

    Update 036_prototype.js

--- javascript/advanced/036_prototype.js
+++ javascript/advanced/036_prototype.js
@@ -1,36 +1,29 @@
-// Prototype & Inheritance
+// Prototype and Inheritance
 
-// constructor function
 function Person(name, age) {
   this.name = name;
-  this.age  = age;
+  this.age = age;
 }
 
-// add method to prototype
 Person.prototype.greet = function() {
-  return `Hi, I'm ${this.name} (${this.age})`;
+  return "Hi, " + this.name + " (" + this.age + ")";
 };
 
 const alice = new Person("Alice", 25);
 console.log(alice.greet());
 
-// prototype chain
-console.log(alice.__proto__ === Person.prototype); // true
-console.log(Person.prototype.__proto__ === Object.prototype); // true
+console.log(alice.__proto__ === Person.prototype);
 
-// Object.create() inheritance
 const animal = {
-  type: "animal",
-  speak() { return `${this.name} makes a sound`; }
+  speak: function() {
+    return this.name + " speaks";
+  }
 };
 
 const dog = Object.create(animal);
-dog.name  = "Rex";
-dog.breed = "Husky";
+dog.name = "Rex";
 console.log(dog.speak());
-console.log(dog.type); // inherited
 
-// constructor-based inheritance
 function Student(name, age, grade) {
   Person.call(this, name, age);
   this.grade = grade;
@@ -40,17 +33,11 @@ Student.prototype = Object.create(Person.prototype);
 Student.prototype.constructor = Student;
 
 Student.prototype.study = function() {
-  return `${this.name} is studying (grade ${this.grade})`;
+  return this.name + " grade " + this.grade;
 };
 
 const bob = new Student("Bob", 17, 2);
-console.log(bob.greet());  // inherited from Person
+
+console.log(bob.greet());
 console.log(bob.study());
-
-// instanceof
-console.log(bob instanceof Student); // true
-console.log(bob instanceof Person);  // true
-
-// hasOwnProperty
-console.log(bob.hasOwnProperty("name"));  // true
-console.log(bob.hasOwnProperty("greet")); // false
+console.log(bob instanceof Person);
