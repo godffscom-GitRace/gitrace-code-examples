f6cd29ca448455409af945be2e0513c5817d551a
godffscom-GitRace <godffscom@gmail.com>
2026-04-26 23:49:34 +0900

    Update 001_HelloWorld.java

--- java/beginner/001_HelloWorld.java
+++ java/beginner/001_HelloWorld.java
@@ -1,24 +1,24 @@
-public class 001_HelloWorld {
+public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Java World!");
+        System.out.println("Hello Java World");
 
-        String msg = greet("김철수");
+        String msg = greet("Alex");
         System.out.println(msg);
 
-        sayHello("이영희");
-        sayHello("박민수");
+        sayHello("Sam");
+        sayHello("Jamie");
 
         int result = add(3, 5);
         System.out.println("3 + 5 = " + result);
     }
 
     public static String greet(String name) {
-        return "안녕하세요, " + name + "님!";
+        return "Hello " + name;
     }
 
     public static void sayHello(String name) {
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hi " + name);
     }
 
     public static int add(int a, int b) {
