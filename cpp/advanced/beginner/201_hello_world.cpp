3f9436e428be4cc1ae3ed6305062a7c2ed99fb2d
godffscom-GitRace <godffscom@gmail.com>
2026-04-27 23:59:52 +0900

    Update 201_hello_world.cpp

--- cpp/advanced/beginner/201_hello_world.cpp
+++ cpp/advanced/beginner/201_hello_world.cpp
@@ -5,16 +5,19 @@
 using namespace std;
 
 int main() {
-    // cout: C++의 표준 출력 스트림
-    cout << "Hello, World!" << endl;
-    cout << "안녕하세요, C++!" << endl;
+    cout << "Hello World" << endl;
+    cout << "Welcome to Cpp" << endl;
 
-    // endl: 줄바꿈 + 버퍼 비우기 (\n보다 안전)
-    cout << "첫 번째 줄" << endl;
-    cout << "두 번째 줄" << "\n";  // \n도 사용 가능
+    cout << "Line one" << endl;
+    cout << "Line two" << "\n";
 
-    // << 연산자로 여러 값을 이어서 출력
-    cout << "C++ " << "is " << "awesome!" << endl;
+    cout << "Cpp " << "is " << "fun" << endl;
+
+    int score = 10;
+    cout << "Score " << score << endl;
+
+    score = score + 5;
+    cout << "New score " << score << endl;
 
     return 0;
 }
