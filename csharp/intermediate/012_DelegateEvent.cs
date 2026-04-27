8935322d9c7f7dc5d2f819059002a5af58a70159
godffscom-GitRace <godffscom@gmail.com>
2026-04-27 23:48:39 +0900

    Update 012_DelegateEvent.cs

--- csharp/intermediate/012_DelegateEvent.cs
+++ csharp/intermediate/012_DelegateEvent.cs
@@ -3,95 +3,51 @@
 
 using System;
 
-namespace DelegateEvent
+delegate int Calc(int a,int b);
+
+class Program
 {
-    // delegate 선언 - 메서드의 시그니처 정의
-    delegate int Calculator(int a, int b);
-    delegate void Notify(string message);
-
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            // === delegate 기본 ===
-            Console.WriteLine("=== Delegate ===");
-            Calculator add = Add;
-            Calculator multiply = Multiply;
+        Calc add = (a,b)=>a+b;
+        Console.WriteLine(add(2,3));
 
-            Console.WriteLine($"더하기: {add(10, 3)}");       // 13
-            Console.WriteLine($"곱하기: {multiply(10, 3)}");  // 30
+        Action<string> n = null;
+        n += m=>Console.WriteLine("a "+m);
+        n += m=>Console.WriteLine("b "+m);
+        n("ok");
 
-            // 람다로 대입
-            Calculator subtract = (a, b) => a - b;
-            Console.WriteLine($"빼기: {subtract(10, 3)}");    // 7
+        var btn = new Btn();
+        btn.Clicked += t=>Console.WriteLine("hit "+t);
+        btn.Click();
 
-            // += 연산자 - 멀티캐스트 대리자
-            Console.WriteLine("\n=== 멀티캐스트 ===");
-            Notify notify = null;
-            notify += msg => Console.WriteLine($"[이메일] {msg}");
-            notify += msg => Console.WriteLine($"[SMS] {msg}");
-            notify += msg => Console.WriteLine($"[푸시] {msg}");
-            notify?.Invoke("새로운 알림!");
-
-            // === event 키워드 ===
-            Console.WriteLine("\n=== Event ===");
-            var button = new Button("확인");
-
-            // 이벤트 구독
-            button.Clicked += (sender, text) =>
-                Console.WriteLine($"버튼 '{text}' 클릭됨!");
-            button.Clicked += (sender, text) =>
-                Console.WriteLine($"로그: {sender} - {text}");
-
-            button.Click();
-            button.Click();
-
-            // 콜백 패턴
-            Console.WriteLine("\n=== 콜백 패턴 ===");
-            var downloader = new FileDownloader();
-            downloader.OnProgress += percent =>
-                Console.WriteLine($"다운로드: {percent}%");
-            downloader.OnComplete += () =>
-                Console.WriteLine("다운로드 완료!");
-
-            downloader.Download("file.zip");
-        }
-
-        static int Add(int a, int b) => a + b;
-        static int Multiply(int a, int b) => a * b;
+        var d = new Down();
+        d.Step += p=>Console.WriteLine(p);
+        d.Done += ()=>Console.WriteLine("end");
+        d.Run();
     }
+}
 
-    // event 키워드 사용
-    class Button
+class Btn
+{
+    public event Action<string> Clicked;
+
+    public void Click()
     {
-        public string Text { get; }
-
-        // event 선언 - 외부에서 직접 Invoke 불가
-        public event Action<string, string> Clicked;
-
-        public Button(string text) { Text = text; }
-
-        public void Click()
-        {
-            Console.WriteLine($"\n[{Text}] 버튼 눌림");
-            Clicked?.Invoke("Button", Text);
-        }
+        Clicked?.Invoke("btn");
     }
+}
 
-    // 콜백 패턴
-    class FileDownloader
+class Down
+{
+    public event Action<int> Step;
+    public event Action Done;
+
+    public void Run()
     {
-        public event Action<int> OnProgress;
-        public event Action OnComplete;
+        for(int i=1;i<=3;i++)
+            Step?.Invoke(i);
 
-        public void Download(string fileName)
-        {
-            Console.WriteLine($"\n'{fileName}' 다운로드 시작");
-            for (int i = 25; i <= 100; i += 25)
-            {
-                OnProgress?.Invoke(i);
-            }
-            OnComplete?.Invoke();
-        }
+        Done?.Invoke();
     }
 }
